9f1e7e29e205cc371883e734120a82b24a170577
Dario <dariomsn@hotmail.it>
2017-07-21 15:58:13 +0200

    bugfix

--- modules/commands/help.py
+++ modules/commands/help.py
@@ -7,7 +7,7 @@ def helpcb(bot,update):
     msg = "===== <b>COMMANDS</b> =====\n"
     for command in config.commands:
         msg += "/{} - {}\n".format(html.escape(command.name), html.escape(command.desc))
-    msg = msg+" ===== <b>CONTROLS</b> =====\n"
+    msg += " ===== <b>CONTROLS</b> =====\n"
     for cont in config.onjoin:
         msg += "<code>{}</code> - <code>{}</code>\n".format(html.escape(cont.name), html.escape(cont.desc))
     update.message.reply_text(text=msg, parse_mode=ParseMode.HTML)
